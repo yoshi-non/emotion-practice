34522fdb97610f32c36f0db50f4015a375191f1e
yoshi-non <himekawa.yoshino44@gmail.com>
2022-05-30 22:13:24 +0900

    TopNavプロトタイプ

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,10 +1,18 @@
 import * as styles from '../styles/TopNav.styles'
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export const TopNav = () => {
   return (
     <div css={styles.container}>
-      <NotificationsIcon />
+      <div css={styles.logo}>Logo</div>
+      <div css={styles.settingBox}>
+        <div css={styles.settingMine}>
+          <img src="http://kai-you.net/images/a/2021/04/267752288aa2d5db2a5ffe6720dfbec6.jpg" alt="iconImg" css={styles.navIconImg} />
+          <ArrowDropDownIcon />
+        </div>
+        <NotificationsIcon css={styles.bell} />
+      </div>
     </div>
   )
 }
--- styles/TopNav.styles.ts
+++ styles/TopNav.styles.ts
@@ -1,7 +1,43 @@
 import { css } from '@emotion/react'
 
 export const container = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 3rem;
   width: 100%;
-  background-color: red;
+  box-shadow: 0 1px 1px rgb(0 0 0 / 20%);
+  background-color: white;
 `
+export const logo = css`
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  background-color: white;
+`
+export const settingBox = css`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`
+export const settingMine = css`
+  display: flex;
+  align-items: center;
+  padding-left: 0.3rem;
+  border-left: 1px solid #ddd;
+`
+export const navIconImg = css`
+  height: 2.5rem;
+  width: 2.5rem;
+  border-radius: 50%;
+  object-fit: cover;
+  background-color: white;
+`
+export const bell = css`
+  height: 2.5rem;
+  width: 2.5rem;
+  border-left: 1px solid #ddd;
+  background-color: white;
+`
+
+
