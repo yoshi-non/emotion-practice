8889fbfd7b2d8c6a468e11f1fc7a7f46c6f4425a
yoshi-non <himekawa.yoshino44@gmail.com>
2022-05-31 00:09:23 +0900

    MainContentデフォルト

---
+++ components/MainGroup.tsx
@@ -0,0 +1,10 @@
+import * as styles from '../styles/MainGroup.styles'
+
+export const MainGroup = () => {
+    return (
+      <div css={styles.container}>
+          fff
+      </div>
+    )
+  }
+  
